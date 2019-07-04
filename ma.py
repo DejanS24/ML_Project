Fixed Moving average

ma.py
@@ -4,184 +4,61 @@ import numpy as np
 import pandas as pd
 # import seaborn as sns
 import time
-
-from datetime import date, datetime, time, timedelta
-# from matplotlib import pyplot as plt
+#
+# from datetime import date, datetime, time, timedelta
+from matplotlib import pyplot as plt
 # from pylab import rcParams
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-# from tqdm import tqdm_notebook
-
-# %matplotlib inline
-
-#### Input params ##################
-stk_path = "googl.us.txt"
-test_size = 0.2                 # proportion of dataset to be used as test set
-cv_size = 0.2                   # proportion of dataset to be used as cross-validation set
-Nmax = 21                       # for feature at day t, we use lags from t-1, t-2, ..., t-N as features
-                                # Nmax is the maximum N we are going to test
-fontsize = 14
-ticklabelsize = 14
-####################################
-
-
-def get_preds_mov_avg(df, target_col, N, pred_min, offset):
-    """
-    Given a dataframe, get prediction at timestep t using values from t-1, t-2, ..., t-N.
-    Using simple moving average.
-    Inputs
-        df         : dataframe with the values you want to predict. Can be of any length.
-        target_col : name of the column you want to predict e.g. 'adj_close'
-        N          : get prediction at timestep t using values from t-1, t-2, ..., t-N
-        pred_min   : all predictions should be >= pred_min
-        offset     : for df we only do predictions for df[offset:]. e.g. offset can be size of training set
-    Outputs
-        pred_list  : list. The predictions for target_col. np.array of length len(df)-offset.
-    """
-    pred_list = df[target_col].rolling(window=N, min_periods=1).mean()  # len(pred_list) = len(df)
-
-    # Add one timestep to the predictions
-    pred_list = np.concatenate((np.array([np.nan]), np.array(pred_list[:-1])))
-
-    # If the values are < pred_min, set it to be pred_min
-    pred_list = np.array(pred_list)
-    pred_list[pred_list < pred_min] = pred_min
-
-    return pred_list[offset:]
-
-
-def get_mape(y_true, y_pred):
-    """
-    Compute mean absolute percentage error (MAPE)
-    """
-    y_true, y_pred = np.array(y_true), np.array(y_pred)
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
-
-
-df = pd.read_csv(stk_path, sep = ",")
-
-# Convert Date column to datetime
-df.loc[:, 'Date'] = pd.to_datetime(df['Date'],format='%Y-%m-%d')
-
-# Change all column headings to be lower case, and remove spacing
-df.columns = [str(x).lower().replace(' ', '_') for x in df.columns]
-
-# Get month of each sample
-df['month'] = df['date'].dt.month
-
-# Sort by datetime
-df.sort_values(by='date', inplace=True, ascending=True)
-
-df.head(10)
-
-
-# Plot adjusted close over time
-# rcParams['figure.figsize'] = 10, 8 # width 10, height 8
-
-# ax = df.plot(x='date', y='adj_close', style='b-', grid=True)
-# ax.set_xlabel("date")
-# ax.set_ylabel("USD")
-
-
-# Get sizes of each of the datasets
-num_cv = int(cv_size*len(df))
-num_test = int(test_size*len(df))
-num_train = len(df) - num_cv - num_test
-print("num_train = " + str(num_train))
-print("num_cv = " + str(num_cv))
-print("num_test = " + str(num_test))
-
-# Split into train, cv, and test
-train = df[:num_train]
-cv = df[num_train:num_train+num_cv]
-train_cv = df[:num_train+num_cv]
-test = df[num_train+num_cv:]
-print("train.shape = " + str(train.shape))
-print("cv.shape = " + str(cv.shape))
-print("train_cv.shape = " + str(train_cv.shape))
-print("test.shape = " + str(test.shape))
-
-
-# Plot adjusted close over time
-# rcParams['figure.figsize'] = 10, 8 # width 10, height 8
-
-# ax = train.plot(x='date', y='adj_close', style='b-', grid=True)
-# ax = cv.plot(x='date', y='adj_close', style='y-', grid=True, ax=ax)
-# ax = test.plot(x='date', y='adj_close', style='g-', grid=True, ax=ax)
-# ax.legend(['train', 'validation', 'test'])
-# ax.set_xlabel("date")
-# ax.set_ylabel("USD")
-
-RMSE = []
-mape = []
-for N in range(1, Nmax + 1):  # N is no. of samples to use to predict the next value
-    est_list = get_preds_mov_avg(train_cv, 'adj_close', N, 0, num_train)
-
-    cv['est' + '_N' + str(N)] = est_list
-    RMSE.append(math.sqrt(mean_squared_error(est_list, cv['adj_close'])))
-    mape.append(get_mape(cv['adj_close'], est_list))
-print('RMSE = ' + str(RMSE))
-print('MAPE = ' + str(mape))
-df.head()
-
-
-
-# Plot RMSE versus N
-# plt.figure(figsize=(12, 8), dpi=80)
-# plt.plot(range(1, Nmax+1), RMSE, 'x-')
-# plt.grid()
-# plt.xlabel('N')
-# plt.ylabel('RMSE')
-# plt.xlim([2, 21])
-# plt.xticks([2, 5, 10, 15, 20])
-
-
-# Plot MAPE versus N. Note for MAPE smaller better.
-# plt.figure(figsize=(12, 8), dpi=80)
-# plt.plot(range(1, Nmax+1), mape, 'x-')
-# plt.grid()
-# plt.xlabel('N')
-# plt.ylabel('MAPE')
-# plt.xlim([2, 21])
-# plt.xticks([2, 5, 10, 15, 20])
-# matplotlib.rcParams.update({'font.size': 14})
-
-
-# Set optimum N
-N_opt = 2
-
-
-# Plot adjusted close over time
-# rcParams['figure.figsize'] = 10, 8 # width 10, height 8
+# from sklearn.linear_model import LinearRegression
+# from sklearn.metrics import mean_squared_error
+# # from tqdm import tqdm_notebook
 #
-# ax = train.plot(x='date', y='adj_close', style='b-', grid=True)
-# ax = cv.plot(x='date', y='adj_close', style='y-', grid=True, ax=ax)
-# ax = test.plot(x='date', y='adj_close', style='g-', grid=True, ax=ax)
-# ax = cv.plot(x='date', y='est_N1', style='r-', grid=True, ax=ax)
-# ax = cv.plot(x='date', y='est_N2', style='m-', grid=True, ax=ax)
-# ax.legend(['train', 'validation', 'test', 'predictions with N=1', 'predictions with N=2'])
-# ax.set_xlabel("date")
-# ax.set_ylabel("USD")
-
-
-# Plot adjusted close over time
-# rcParams['figure.figsize'] = 10, 8 # width 10, height 8
+# # %matplotlib inline
 #
-# ax = train.plot(x='date', y='adj_close', style='bx-', grid=True)
-# ax = cv.plot(x='date', y='adj_close', style='yx-', grid=True, ax=ax)
-# ax = test.plot(x='date', y='adj_close', style='gx-', grid=True, ax=ax)
-# ax = cv.plot(x='date', y='est_N1', style='rx-', grid=True, ax=ax)
-# ax = cv.plot(x='date', y='est_N2', style='mx-', grid=True, ax=ax)
-# ax.legend(['train', 'validation', 'test', 'predictions with N=1', 'predictions with N=2'])
-# ax.set_xlabel("date")
-# ax.set_ylabel("USD")
-# ax.set_xlim([date(2017, 11, 1), date(2017, 12, 30)])
-# ax.set_ylim([127, 137])
-# ax.set_title('Zoom in to dev set')
+# #### Input params ##################
+# stk_path = "googl.us.txt"
+# test_size = 0.2                 # proportion of dataset to be used as test set
+# cv_size = 0.2                   # proportion of dataset to be used as cross-validation set
+# Nmax = 21                       # for feature at day t, we use lags from t-1, t-2, ..., t-N as features
+#                                 # Nmax is the maximum N we are going to test
+# fontsize = 14
+# ticklabelsize = 14
+# ####################################
 
 
-est_list = get_preds_mov_avg(df, 'adj_close', N_opt, 0, num_train+num_cv)
-test['est' + '_N' + str(N_opt)] = est_list
-print("RMSE = %0.3f" % math.sqrt(mean_squared_error(est_list, test['adj_close'])))
-print("MAPE = %0.3f%%" % get_mape(test['adj_close'], est_list))
-test.head()
+def predict_ma(df):
+
+    l = len(df)
+    if l < 4293:
+        train = df[:round(l * 4 / 5)]
+        test = df[round(l * 4 / 5):]
+    else:
+        train = df[-4293:l - 973]
+        test = df[-973:]
+
+    # Aggregating the dataset at daily level
+    df['Timestamp'] = pd.to_datetime(df.Date, format='%Y-%m-%d')
+    df.index = df.Timestamp
+    # df = df.resample('Close').mean()
+    train['Timestamp'] = pd.to_datetime(train.Date, format='%Y-%m-%d')
+    train.index = train.Timestamp
+    # train = train.resample('Close').mean()
+    test['Timestamp'] = pd.to_datetime(test.Date, format='%Y-%m-%d')
+    test.index = test.Timestamp
+    # test = test.resample('Close').mean()
+
+
+    # # Plotting data
+    # train.Close.plot(figsize=(15, 8), title='Daily Ridership', fontsize=14)
+    # test.Close.plot(figsize=(15, 8), title='Daily Ridership', fontsize=14)
+    # plt.show()
+
+    y_hat_avg = test.copy()
+    y_hat_avg['moving_avg_forecast'] = train['Close'].rolling(60).mean().iloc[-1]
+    plt.figure(figsize=(16, 8))
+    plt.plot(train['Close'], label='Train')
+    plt.plot(test['Close'], label='Test')
+    plt.plot(y_hat_avg['moving_avg_forecast'], label='Moving Average Forecast')
+    plt.legend(loc='best')
+    plt.show()
+
+    return y_hat_avg['moving_avg_forecast']